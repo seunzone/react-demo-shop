c0f72bdabaa642e1d9b6da3ed621b269c5f4f854
seunzone <darealseun@gmail.com>
2019-01-25 14:06:59 +0100

    create navbar using styled components :fire:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
+
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to='/'>
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -17,13 +20,23 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to='/cart' className="ml-auto">
-          <button>
-          <i className="fas fa-shopping-cart" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-shopping-cart" />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link> 
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite) !important;
+  font-size: 1.3rem;
+  text-transform: capitalize;
+}
+`
