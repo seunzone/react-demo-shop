2df07feeb8b1e5fd63958390c4a7e2c4a833ff35
seunzone <darealseun@gmail.com>
2019-02-27 06:46:00 +0100

    create cart item components :fire:

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
+import CartList from './CartList';
 
 
 export default class componentName extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart" />
-                <CartColumns />
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if (cart.length > 0) {
+                            return (
+                                <Fragment>
+                                    <Title name="your" title="cart" />
+                                    <CartColumns />
+                                    <CartList value={value} />
+                                </Fragment>
+                            )
+                        } else {
+                            return (
+                                <EmptyCart />
+                            )
+                        }
+                    }}
+                </ProductConsumer>
             </section>
         )
     }
