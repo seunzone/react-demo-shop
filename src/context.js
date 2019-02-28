91cb131760589dc8db0d4f8b79873997e90509c1
seunzone <darealseun@gmail.com>
2019-02-28 17:26:56 +0100

    increment and decrement cart items

--- src/context.js
+++ src/context.js
@@ -73,7 +73,19 @@ class ProductProvider extends Component {
         }, ()=>{this.addTotals();})
     }
     decrement = (id) =>{
-        console.log('decrement me sir');
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=> item.id === id)
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count - 1;
+        if(product.count === 0){
+            this.removeItem(id);
+        } else {
+            product.total = product.count * product.price;
+            this.setState(()=>{
+                return {cart:[...tempCart]}
+            }, ()=>{this.addTotals();})
+        }
     }
     removeItem = (id) =>{
         let tempProducts = [...this.state.products];
