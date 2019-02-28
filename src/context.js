51a4274406f53a18cbdef698636529e219f36342
seunzone <darealseun@gmail.com>
2019-02-28 15:51:35 +0100

    remove item from cart

--- src/context.js
+++ src/context.js
@@ -68,7 +68,23 @@ class ProductProvider extends Component {
         console.log('decrement me sir');
     }
     removeItem = (id) =>{
-        console.log('remove me sir');
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.InCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=>{
+            return {
+                cart:[...tempCart],
+                products: [...tempProducts]
+            }
+        }, ()=>{
+            this.addTotals();
+        })
     }
     clearCart = () =>{
         this.setState(()=>{
